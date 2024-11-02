f8fb426288b27b54a8540cabddcca00538f4c394
Atharv-28 <atharvmt@outlook.com>
2024-11-02 23:02:03 +0530

    Routes added to those components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './Pages/home';
 import Login from './Pages/login';
 import Signup from './Pages/signup';
+import Feed from './Components/Feed/feed';
+import Lists from './Components/Lists/lists'; 
+import Profile from './Components/Profile/profile'; 
+import Bookmark from './Components/Bookmark/bookmark';
+import Messages from './Components/Messages/messages';  
+import Notification from './Components/Notification/notification';  
+import Explore from './Components/Explore/explore'; 
+import More from './Components/More/more';  
+
 
 function App() {
   return (
@@ -13,6 +21,15 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="feed" element={<Feed />} />
+        <Route path="lists" element={<Lists />} />  
+        <Route path="profile" element={<Profile />} />  
+        <Route path="bookmark" element={<Bookmark />} />  
+        <Route path="messages" element={<Messages />} />  
+        <Route path="notification" element={<Notification />} />  
+        <Route path="explore" element={<Explore />} />  
+        <Route path="more" element={<More />} />  
+
       </Routes>
     </div>
   );
