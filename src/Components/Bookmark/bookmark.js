e20e0a245199920856c7bcf167640a8e5a8885e0
Atharv-28 <atharvmt@outlook.com>
2024-11-02 22:57:17 +0530

    Adding directories of each components

---
+++ src/Components/Bookmark/bookmark.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Bookmark = () => {
+  return (
+    <div>bookmark</div>
+  )
+}
+
+export default Bookmark
---
+++ src/Components/Feed/feed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './feed.css';
+
+const Feed = () => {
+    return (
+        <div>
+            <h1>Feed Component</h1>
+            <p>This is the feed component.</p>
+        </div>
+    );
+};
+
+export default Feed;
