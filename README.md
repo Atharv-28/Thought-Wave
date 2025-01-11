aa4cf1dddb4ac0098703e69e016c7cc0c4e17721
Atharv-28 <atharvmt@outlook.com>
2025-01-11 23:22:36 +0530

    Unnessesary files removed and Ext Features listed in Readme

--- README.md
+++ README.md
@@ -13,3 +13,11 @@
 
 # Theme (Color Pallete)
 1. Primary - 51c7c8
+
+
+Advanced Search Filters: Allow users to search for tweets based on various criteria like date, location, hashtags, and user mentions.
+
+Voice Tweets: Enable users to record and post voice tweets.
+
+Tweet Scheduling: Allow users to schedule tweets to be posted at a later time.
+
--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ import Home from './pages/home';
 import Login from './pages/login';
 import Signup from './pages/signup';
 import Feed from './Components/Feed/feed';
-import Lists from './Components/Lists/lists'; 
 import Profile from './Components/Profile/profile'; 
 import Bookmark from './Components/Bookmark/bookmark';
-import Message from './Components/Messages/message';  
 import Notification from './Components/Notification/notification';  
 import Explore from './Components/Explore/explore'; 
-import More from './Components/More/more';  
 
 
 function App() {
@@ -22,14 +19,10 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="feed" element={<Feed />} />
-        <Route path="lists" element={<Lists />} />  
         <Route path="profile" element={<Profile />} />  
         <Route path="bookmark" element={<Bookmark />} />  
-        <Route path="messages" element={<Message />} />  
         <Route path="notification" element={<Notification />} />  
         <Route path="explore" element={<Explore />} />  
-        <Route path="more" element={<More />} />  
-
       </Routes>
     </div>
   );
